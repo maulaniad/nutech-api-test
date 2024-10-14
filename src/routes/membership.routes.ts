8936a2f841eba4f899a6a56859010439636c9063
Adrian Maulani <maulani.ad@gmail.com>
2024-10-14 23:43:45 +0700

    feat: new get profile

--- src/routes/membership.routes.ts
+++ src/routes/membership.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from "express";
 
-import { createMembership, loginMembership } from "@controllers/membership";
+import {
+    membershipRegistration,
+    membershipLogin,
+    membershipProfile
+} from "@controllers/membership";
 import {
     validateMembershipRegistrationPayload,
     validateMembershipLoginPayload,
 } from "@middlewares/membership/validation";
+import { jwtAuth } from "@middlewares/auth";
 
 
 const router = Router();
 
-router.post("/registration", [validateMembershipRegistrationPayload], createMembership);
-router.post("/login", [validateMembershipLoginPayload], loginMembership);
+router.post("/registration", [validateMembershipRegistrationPayload], membershipRegistration);
+router.post("/login", [validateMembershipLoginPayload], membershipLogin);
+router.get("/profile", [jwtAuth], membershipProfile);
 
 export default router;
