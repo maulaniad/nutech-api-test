edbb453346446486ebea30c6c3a4f7eee8fffaaa
Adrian Maulani <maulani.ad@gmail.com>
2024-10-14 21:19:24 +0700

    refactor: simplify repo return values

--- src/repositories/membership.ts
+++ src/repositories/membership.ts
@@ -3,7 +3,7 @@ import db from "@configs/database";
 
 class MembershipRepo {
     static readonly getUser = async (param: string, value: any) => {
-        const sql = `SELECT oid, email, first_name, last_name, avatar FROM users WHERE "${param}" = $1`;
+        const sql = `SELECT oid, email, first_name, last_name, profile_image FROM users WHERE "${param}" = $1`;
         const params = [value];
 
         const result = await db.query(sql, params);
@@ -12,34 +12,20 @@ class MembershipRepo {
 
     static readonly getMembershipByEmail = async (email: string) => {
         const result = await this.getUser("email", email);
-
         if (result.rows.length <= 0) {
             return null;
         }
 
-        return {
-            oid: result.rows[0].oid,
-            email: result.rows[0].email,
-            firstName: result.rows[0].first_name,
-            lastName: result.rows[0].last_name,
-            profileImage: result.rows[0].avatar
-        };
+        return result.rows[0];
     }
 
     static readonly getMembershipByOid = async (oid: string) => {
         const result = await this.getUser("oid", oid);
-
         if (result.rows.length <= 0) {
             return null;
         }
 
-        return {
-            oid: result.rows[0].oid,
-            email: result.rows[0].email,
-            firstName: result.rows[0].first_name,
-            lastName: result.rows[0].last_name,
-            profileImage: result.rows[0].avatar
-        };
+        return result.rows[0]
     }
 
     static readonly createMembership = async (
@@ -48,22 +34,19 @@ class MembershipRepo {
         lastName: string,
         password: string
     ) => {
-        const sql = "INSERT INTO users (email, first_name, last_name, password) VALUES ($1, $2, $3, $4) RETURNING *";
+        const sql = `
+            INSERT INTO users (email, first_name, last_name, password)
+            VALUES ($1, $2, $3, $4)
+            RETURNING oid, email, first_name, last_name, profile_image
+        `;
         const params = [email, firstName, lastName, password];
 
         const result = await db.query(sql, params);
-
         if (result.rows.length <= 0) {
             return null;
         }
 
-        return {
-            oid: result.rows[0].oid,
-            email: result.rows[0].email,
-            firstName: result.rows[0].first_name,
-            lastName: result.rows[0].last_name,
-            profileImage: result.rows[0].avatar
-        };
+        return result.rows[0];
     };
 }
 
