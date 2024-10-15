238b6d9e6fabd99f9cf291e491951a331a326a37
Adrian Maulani <maulani.ad@gmail.com>
2024-10-15 13:31:08 +0700

    fix: auth middleware did not stop when error

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -14,6 +14,7 @@ const jwtAuth = (req: Request, res: Response, next: NextFunction) => {
     const validToken = verifyToken(token);
     if (!validToken) {
         sendResponse(res, null, 401, "Token tidak valid atau kedaluwarsa");
+        return;
     }
 
     req.user = validToken;
