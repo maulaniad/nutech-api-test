cba2a471e9d1809a3751203e075dc4796d04e253
Adrian Maulani <maulani.ad@gmail.com>
2024-10-17 22:33:50 +0700

    chore: required data for database migration

---
+++ database/data/banners.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.banners ("oid",banner_name,banner_image,description,created_at,updated_at,deleted_at) VALUES
+	 ('68f529d4-8e83-4664-806c-92d16ec40ac0'::uuid,'Banner 1','uploads/dummy.png','Oh yeah monster on the loose!','2024-10-16 19:23:18.358293','2024-10-16 19:23:18.358293',NULL),
+	 ('613ea491-3a16-4244-aae1-16a0a58b67e9'::uuid,'Banner 2','uploads/dummy.png','Your duty is not over!','2024-10-16 19:23:18.358293','2024-10-16 19:23:18.358293',NULL),
+	 ('d2f83181-c33f-456d-8f8d-4984e0bc3f75'::uuid,'Banner 3','uploads/dummy.png','You are powerless!','2024-10-16 19:23:18.358293','2024-10-16 19:23:18.358293',NULL),
+	 ('2df2c049-334d-4ad8-8e7e-7ae944553a0f'::uuid,'Banner 4','uploads/dummy.png','Get out of my way!','2024-10-16 19:23:18.358293','2024-10-16 19:23:18.358293',NULL),
+	 ('5cf4d613-7e25-40f1-b3d6-b2545f07bf1e'::uuid,'Banner 5','uploads/dummy.png','Face your fear!','2024-10-16 19:23:18.358293','2024-10-16 19:23:18.358293',NULL),
+	 ('5f4929db-41b3-4e9b-82db-6d2f220634e1'::uuid,'Banner 6','uploads/dummy.png','Watch them run!','2024-10-16 19:23:18.358293','2024-10-16 19:23:18.358293',NULL);
---
+++ database/data/services.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.services ("oid",service_code,service_name,service_icon,service_tariff,created_at,updated_at,deleted_at) VALUES
+	 ('8c30fdde-d64a-43ac-9c07-f92d836b73ce'::uuid,'PAJAK','Pajak PBB','uploads/dummy.png',40000,'2024-10-16 20:08:09.584348','2024-10-16 20:08:09.584348',NULL),
+	 ('8722933b-de75-4736-8594-5949a5bbed50'::uuid,'PLN','Listrik','uploads/dummy.png',10000,'2024-10-16 20:08:09.589714','2024-10-16 20:08:09.589714',NULL),
+	 ('4b199d01-8eb1-419a-9e88-ff4576dba053'::uuid,'PDAM','PDAM Berlangganan','uploads/dummy.png',40000,'2024-10-16 20:08:09.591955','2024-10-16 20:08:09.591955',NULL),
+	 ('8144db2c-8add-48ca-92eb-23350744fb8a'::uuid,'PULSA','Pulsa','uploads/dummy.png',40000,'2024-10-16 20:08:09.594445','2024-10-16 20:08:09.594445',NULL),
+	 ('6befc378-28d4-4827-9912-0dad80f043bb'::uuid,'PGN','PGN Berlangganan','uploads/dummy.png',50000,'2024-10-16 20:08:09.597087','2024-10-16 20:08:09.597087',NULL),
+	 ('c8dad733-e8ec-413e-9ec7-71e0098e51d2'::uuid,'MUSIK','Musik Berlangganan','uploads/dummy.png',50000,'2024-10-16 20:08:09.599821','2024-10-16 20:08:09.599821',NULL),
+	 ('9ba754c9-bd4d-4b75-8d82-69a02725d609'::uuid,'TV','TV Berlangganan','uploads/dummy.png',50000,'2024-10-16 20:08:09.602063','2024-10-16 20:08:09.602063',NULL),
+	 ('6f942fcb-303b-474c-8e58-882b67d880d6'::uuid,'PAKET_DATA','Paket Data','uploads/dummy.png',50000,'2024-10-16 20:08:09.603749','2024-10-16 20:08:09.603749',NULL),
+	 ('a1540e0c-6950-4f2e-ab53-f411df86a597'::uuid,'VOUCHER_GAME','Voucher Game','uploads/dummy.png',100000,'2024-10-16 20:08:09.605551','2024-10-16 20:08:09.605551',NULL),
+	 ('d59964a8-8a77-409a-a920-5c843a6733ba'::uuid,'VOUCHER_MAKANAN','Voucher Makanan','uploads/dummy.png',100000,'2024-10-16 20:08:09.607883','2024-10-16 20:08:09.607883',NULL);
+INSERT INTO public.services ("oid",service_code,service_name,service_icon,service_tariff,created_at,updated_at,deleted_at) VALUES
+	 ('33152b6f-f943-4b2d-b8d2-ee22e03fc09d'::uuid,'QURBAN','Qurban','uploads/dummy.png',200000,'2024-10-16 20:08:09.609714','2024-10-16 20:08:09.609714',NULL),
+	 ('ef8e315e-648a-45c1-a419-deb743cfd468'::uuid,'ZAKAT','Zakat','uploads/dummy.png',300000,'2024-10-16 20:08:09.611535','2024-10-16 20:08:09.611535',NULL),
+	 ('b674fb2e-9b86-426f-be8b-20f9226dfa0a'::uuid,'TOPUP','Top Up Wallet','uploads/dummy.png',10000,'2024-10-17 20:16:44.825323','2024-10-17 20:16:44.825323',NULL);
